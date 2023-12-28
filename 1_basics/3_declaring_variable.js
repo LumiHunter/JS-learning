814f95cb51424af95c1679758be56c79255ea158
LumiHunter <hwangje36@gmail.com>
2023-12-28 20:41:11 +0900

    first commit

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,28 @@
+/**
+ * 1) var: 더 이상 쓰지 않는다.
+ * 2) let:
+ */
+
+var name = "CodeFactory";
+console.log(name);
+
+let ive = "아이브";
+console.log(ive);
+ive = "안유진";
+
+const newJeans = "뉴진스";
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후에 변경할 수 있으며,
+ * 값을 할당하지 않으면 undefined
+ *
+ * const는 값을 추후 변경할 수 없으며,
+ * 초기값을 할당하지 않으면 안된다.
+ */
+
+/**
+ * 선언과 할당
+ * 1) 변수를 선언하는 것은 'var name'
+ * 2) 할당하는 것은 'var name = '코드팩토리';'
+ */
